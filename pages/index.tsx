ab6cef63c490107dfd2b3ea39227f199b4a69c48
Manuel Zuñiga <manuelzuniga.w@gmail.com>
2022-06-14 15:04:52 -0500

    nvidea view

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,9 +7,9 @@ import type {
 import { useState } from "react";
 
 const imageLink =
-"http://ddragon.leagueoflegends.com/cdn/12.11.1/img/champion/";
+  "http://ddragon.leagueoflegends.com/cdn/12.11.1/img/champion/";
 const imageLoader =
-"https://ddragon.leagueoflegends.com/cdn/img/champion/loading/";
+  "https://ddragon.leagueoflegends.com/cdn/img/champion/loading/";
 
 //{count >= 159 && (setCount(0))}
 
@@ -19,44 +19,44 @@ const Home: NextPage<InferGetStaticPropsType<typeof getStaticProps>> = ({
 
   const [count, setCount] = useState(-1)
   {
-    count >= 155 && count <= -2 && (setCount(-1))
-    }
-    {
+    count >= 155 && (setCount(-1))
+  }
+  {
     count <= -2 && (setCount(154))
-    }
+  }
 
   return (
 
-    <div className="bg-red-700">
 
-<div className="flex flew-row w-full px-6 space-x-4 rounded bg-white">
-      {
-      
-      personajes.map((champ, index) => (
-        
-        index > 0 + count && index < 6 + count && (
+    <div className="flex items-center align-middle bg-black overflow-hidden">
 
-          <div key={champ.key} className="w-1/5 rounded border-2 border-black items-center align-middle">
+      <div className="grid grid-cols-7 gap-4 grid-rows-4 items-center align-middle place-items-center">
+
+      <div className="scale-75 -skew-y-12 w-1/2 h-full row-span-4 col-span-1 rounded bg-lime-700 overflow-hidden hover:origin-bottom-left" onClick={() => setCount(count - 1)}></div>
+        {
+
+          personajes.map((champ, index) => (
+
+            index > 0 + count && index < 6 + count && (
+
+              <div key={champ.key} className="row-span-4 col-span-1 rounded items-center align-middle overflow-hidden">
+
+                <div className="relative overflow-clip text-center items-center align-middle">
+                  <img className="object-fill mx-auto items-center align-middle overflow-clip hover:scale-150" src={imageLoader.concat(champ.id, "_0.jpg")}></img>
+                  <span className="absolute px-2 bottom-1 left-1 font-bold text-white">{champ.name}</span>
+
+                </div>
+
+              </div>
+
+
+            )
+          ))}
+          <div className="scale-75 skew-y-12 w-1/2 h-full row-span-4 col-span-1 rounded bg-lime-700 overflow-hidden hover:origin-bottom-left" onClick={() => setCount(count - 1)}>
             
-          <div className="relative bg-slate-50 overflow-clip text-center items-center align-middle">
-          <img className="overflow-clip hover:scale-150" src={imageLoader.concat(champ.id,"_0.jpg")}></img>
-          <span className="absolute px-2 bottom-0 left-0 font-bold text-white">{champ.name}</span>
-          
           </div>
-        
-          </div>
-          
+      </div>
 
-        )
-      ))}
-</div>
-
-<button onClick={() => setCount(count + 1)}>
-       Click Here + {count} 
-      </button>
-      <button onClick={() => setCount(count - 1)}>
-       Click Here - {count} 
-      </button>
     </div>
   );
 };
